b86452674ba5729c687eb59a7f3036c23cc1a643
Joel Dickel <joeldickel@gmail.com>
2020-06-08 19:07:49 -0300

    First controller and Razor pages test

--- ProjetoWeb/Controllers/HomeController.cs
+++ ProjetoWeb/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace ProjetoWeb.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Course C# Web MVC";
+            ViewData["Studant"] = "Joel";
 
             return View();
         }
