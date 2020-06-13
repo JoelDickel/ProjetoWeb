fc62e4a0d7d0486ed0397ebb13583aac8b38c876
Joel Dickel <joeldickel@gmail.com>
2020-06-13 14:46:47 -0300

    Implementing simple search

---
+++ ProjetoWeb/Services/SalesRecordService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoWeb.Data;
+using ProjetoWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoWeb.Services
+{
+    public class SalesRecordService
+    {
+        private readonly ProjetoWebContext _context;
+
+        public SalesRecordService(ProjetoWebContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SaleRecord>> FindByDate(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SaleRecords select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            return await result.Include(x => x.Seller).Include(x => x.Seller.Department).OrderByDescending(x => x.Date).ToListAsync();
+        }
+    }
+}
--- ProjetoWeb/Startup.cs
+++ ProjetoWeb/Startup.cs
@@ -51,6 +51,7 @@ namespace ProjetoWeb
 
             services.AddScoped<DepartmentService>();
 
+            services.AddScoped<SalesRecordService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
