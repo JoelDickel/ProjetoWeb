a57bc7639511ae4ca7c3a7c5adb1ea438b23e14b
Joel Dickel <joeldickel@gmail.com>
2020-06-13 14:12:03 -0300

    Exception handling for delete (referential integrity)

--- ProjetoWeb/Controllers/SellerController.cs
+++ ProjetoWeb/Controllers/SellerController.cs
@@ -68,8 +68,15 @@ namespace ProjetoWeb.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
